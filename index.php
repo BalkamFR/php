e9d8f1185ca368f1a77518fc7f14c15ec91e1304
PILAZ Pacome <pacome.pilaz@GB42.local>
2023-04-03 15:04:33 +0200

    pacome

--- index.php
+++ index.php
@@ -4,10 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <p>TEST</p>
-    <p>Pacôme PILaz</p>
+    <header>    
+        <img src="" alt="" class="logo">
+        <div class="menu">
+            <a href="">Accueil</a>
+            <a href="">Jeux</a>
+            <a href="">Contat</a>
+        </div>
+        <div>
+            <a href="">Connexion</a>
+            <a href="">Inscription</a>
+        </div>
+    </header>
 </body>
 </html>
