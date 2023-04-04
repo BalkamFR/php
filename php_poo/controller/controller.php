441ad8410ad7dcf171ada18972cac705812ea1c5
MARIJNISSEN Thibault <thibault.marijnissen@GB42.local>
2023-04-04 16:47:39 +0200

    dzdz

--- php_poo/controller/controller.php
+++ php_poo/controller/controller.php
@@ -1,7 +1,17 @@
 <?php 
 
     class PersonnageController(){
-        
+        private Personnage $listePerso = array();
+
+        // public function __construct(p){
+
+        // }
+        public function addPerso($nom,$pv,$race, $force, $niveau){
+            Personnage $p = new Personnage($nom, $pv,$race,$force,$niveau);
+            array_push($listePerso, $p);
+            PersonnageVue $vue = new PersonnageVue($listePerso);
+            $vue->afficher();
+        }
     }
 
 ?>
