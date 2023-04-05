7092762c7a4354bdc19df30c01d9d5d80f46d50a
BalkamFR <113695103+BalkamFR@users.noreply.github.com>
2023-04-05 14:48:06 +0200

    Update controller.php

--- php_poo/controller/controller.php
+++ php_poo/controller/controller.php
@@ -1,16 +1,24 @@
 <?php 
-
+     include '../data_base.php';
     class PersonnageController(){
         private Personnage $listePerso = array();
 
-        // public function __construct(p){
+        public function __construct(p){
 
-        // }
+        }
         public function addPerso($nom,$pv,$race, $force, $niveau){
             Personnage $p = new Personnage($nom, $pv,$race,$force,$niveau);
             array_push($listePerso, $p);
             PersonnageVue $vue = new PersonnageVue($listePerso);
             $vue->afficher();
+            $req = $db -> prepare("INSERT INTO personnage(nom, pv, race, force ,niveau) VALUES(:nom, :pv, :race, :force ,:niveau )");
+          $req->execute([
+              'nom'=> $nom,
+              'pv'=> $pv,
+              'race'=>$race,
+              'force'=> $force,
+              'niveau'=>$niveau,
+          ]);
         }
     }
 
